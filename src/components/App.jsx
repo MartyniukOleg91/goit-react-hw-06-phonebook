f7eff2224bfb83862daf08ed321d5f08b0777bb4
MartyniukOleg91 <111987629+MartyniukOleg91@users.noreply.github.com>
2023-05-18 10:28:00 +0300

    add start files from 4 HW

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,77 @@
-export const App = () => {
+import React, { useState, useEffect } from 'react';
+import { nanoid } from 'nanoid';
+import ContactForm from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
+import { ContactList } from './ContactList/ContactList';
+
+const initialState = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
+
+export default function App() {
+  const [contacts, setContacts] = useState(
+    () => JSON.parse(localStorage.getItem('contactList')) ?? initialState
+  );
+
+  const [filter, setFilter] = useState('');
+  useEffect(() => {
+    localStorage.setItem('contactList', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const handleChange = e => {
+    const { value } = e.target;
+    setFilter(value);
+  };
+
+  const handleSubmit = date => {
+    const id = nanoid();
+    const name = date.name;
+    const number = date.number;
+    const contactsLists = [...contacts];
+
+    if (contactsLists.findIndex(contact => name === contact.name) !== -1) {
+      alert(`${name} is already in contacts.`);
+    } else {
+      contactsLists.push({ id, name, number });
+    }
+
+    setContacts(contactsLists);
+  };
+
+  const handleDelete = date => {
+    setContacts(contacts.filter(contact => contact.id !== date));
+  };
+
+  const getFilteredContacts = () => {
+    const filterContactsList = contacts.filter(contact => {
+      return contact.name.toLowerCase().includes(filter.toLowerCase());
+    });
+    return filterContactsList;
+  };
+
   return (
     <div
       style={{
         height: '100vh',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
-        fontSize: 40,
+        fontSize: 20,
         color: '#010101',
       }}
     >
-      React
+      <h1>Phonebook</h1>
+      <ContactForm handleSubmit={handleSubmit} />
+      <h2> Contacts</h2>
+      <Filter filter={filter} handleChange={handleChange} />
+      <ContactList
+        contacts={getFilteredContacts()}
+        handleDelete={handleDelete}
+      />
     </div>
   );
-};
+}
