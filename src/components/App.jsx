ef0554ddc687f984ff235a07e48f2b3b28fc8be4
MartyniukOleg91 <111987629+MartyniukOleg91@users.noreply.github.com>
2023-05-19 20:20:28 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,8 @@
-import React, { useState } from 'react';
 import ContactForm from './ContactForm/ContactForm';
 import Filter from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
-import { useSelector } from 'react-redux';
-import { getFilterValue } from 'store/selectors';
-import { getContacts } from 'store/selectors';
 
 export default function App() {
-  const filterValue = useSelector(getFilterValue);
-  const contactValue = useSelector(getContacts);
-
-  const [filter, setFilter] = useState('');
-
-  const handleChange = e => {
-    setFilter(filterValue);
-  };
-
-  const getFilteredContacts = () => {
-    const filterContactsList = contactValue.filter(contact => {
-      return contact.name.toLowerCase().includes(filterValue.toLowerCase());
-    });
-    return filterContactsList;
-  };
-
   return (
     <div
       style={{
@@ -38,8 +18,8 @@ export default function App() {
       <h1>Phonebook</h1>
       <ContactForm />
       <h2> Contacts</h2>
-      <Filter filter={filter} handleChange={handleChange} />
-      <ContactList contacts={getFilteredContacts()} />
+      <Filter />
+      <ContactList />
     </div>
   );
 }
